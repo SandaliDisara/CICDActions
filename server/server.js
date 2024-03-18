3005094db7df91a5191d682afcdf9be3a6cc87f1
Sandali Disara <sandalidissanayake123@gmail.com>
2024-03-18 17:53:52 +0530

    Intialize the project

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+const URL = process.env.MONGODB_URL;   //MongoDB URL connection string
+
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to database', err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port number: ${PORT} !`);
+});
